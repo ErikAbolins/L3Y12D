19f778b01dcfa7b0effc8ab44ae1032f54ee4aa2
ABO24002278 - Erik Abolins <ABO24002278@HeartofYorkshire.ac.uk>
2024-10-17 15:57:44 +0100

    lerp camera and double jump

--- lol/Assets/Scripts/Movement.cs
+++ lol/Assets/Scripts/Movement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
+    private int extraJump;
+    public int extraJumpValue; 
     public float moveSpeed = 5f; // Speed of the player
     public float jumpForce = 10f; // Jump force
     public Transform groundCheck; // Reference point for ground check
@@ -12,7 +14,9 @@ public class Movement : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isGrounded;
+    
 
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,6 +26,8 @@ public class Movement : MonoBehaviour
     {
         Move();
         Jump();
+
+
     }
 
     void Move()
@@ -32,16 +38,33 @@ public class Movement : MonoBehaviour
 
     void Jump()
     {
+
+        if (isGrounded == true)
+        {
+            extraJump = extraJumpValue;
+        }
+        
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
 
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             Debug.Log("Jumping");
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+           
+        }
+
+        else if (Input.GetButtonDown("Jump") && extraJump > 0)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            extraJump--;
         }
         else if(!isGrounded)
         {
             Debug.Log("Not grounded");
         }
     }
+
+
+
 }
---
+++ lol/Assets/Scripts/camerafollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float speed;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 desiredPos = player.position + offset; 
+        transform.position = Vector3.Lerp(transform.position, desiredPos, speed * Time.deltaTime);
+    }
+}
+ 
